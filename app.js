b0a2b49c8bc9da7efe4a380fe5b5950ec5b586f4
shubham <shubham.navale@coradius.io>
2019-11-22 06:01:26 +0530

    add app file

---
+++ app.js
@@ -0,0 +1,24 @@
+var express = require('express')
+var app = express()
+
+// Create routes
+app.get('/', function(req, res) {
+  res.send('Hi there')
+})
+
+app.get('/bye', function(req, res) {
+  res.send('Good Bye')
+})
+
+app.get('/dog', function(req, res) {
+  res.send('Meow!!!')
+})
+
+app.get('*', function(req, res) {
+  res.send('You are a star')
+})
+
+// Tell express to listen for requests
+app.listen('3000', '192.168.43.143', function() {
+  console.log('Server has started on port: ', process.env.PORT)
+})
